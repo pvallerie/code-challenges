c5404340bd84b5bd52f7d4f68ed79fa6cc0a957f
pvallerie <petevallerie@gmail.com>
2021-05-25 20:41:56 -0400

    Edit fetch file

--- Problems/APIRequests/fetch.js
+++ Problems/APIRequests/fetch.js
@@ -1,8 +1,17 @@
-const apiCall = url => {
-    fetch(url)
-        .then(res => res.json())
+
+
+const apiCall = (url, id) => {
+    fetch(`${url}/${id}`)
+        .then(res => {
+            if (!res.ok){
+                throw new Error(res.status)
+            }
+            return res.json()
+        })
         .then(data => console.log(data))
+        .catch(error => console.error('Error with status:', error))
 }
 
-const jsonplaceholder = 'https://jsonplaceholder.typicode.com/users'
-apiCall(jsonplaceholder)
+const id = 1
+const url = 'https://jsonplaceholder.typicode.com/users'
+apiCall(url, id)
